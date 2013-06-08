edb7c86593c0cd6b573b1c4873a59baa8648c855
Greg Tutas <tutasg@comcast.net>
2013-06-08 18:02:59 -0400

    example ch.5

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -11,6 +11,26 @@ subject { page }
       it { should have_selector('title',
 	     text: full_title('') ) }
       it { should_not have_selector('title', text: full_title('home') ) }
+
+  it "should have the right links on the layout" do
+    visit root_path
+    click_link "About"
+    page.should have_selector 'title', text: full_title('About')
+    click_link "Help"
+    page.should have_selector 'title', text: full_title('Help')
+
+    click_link "Contact"
+    page.should have_selector 'title', text: full_title('Contact')
+    click_link "Home"
+    click_link "Sign up now!"
+    page.should have_selector 'title', text: full_title('Sign up')
+    click_link "sample app"
+    page.should have_selector 'title', text: full_title('')
+  end
+
+
+
+
   end
    
    describe "Help page" do
