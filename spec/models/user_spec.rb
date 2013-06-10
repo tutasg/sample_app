2a71e33f381fd7c3ea7899c21b4827ba5ee1f88c
Greg Tutas <tutasg@comcast.net>
2013-06-10 19:52:55 -0400

    finished 8.2

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -22,8 +22,15 @@ describe User do
   it { should respond_to(:password_digest) }
   it { should respond_to(:password)}
   it { should respond_to(:password_confirmation)}
-
+  it { should respond_to(:remember_token)}
   it { should be_valid}
+
+ describe "remember token" do
+    before { @user.save }
+    its(:remember_token) { should_not be_blank }
+  end
+
+
 
 describe "return value of authenticate method" do
   before { @user.save }
