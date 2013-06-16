c3dd46a0345fa46540bc85514df63d0c9901a2ed
Greg Tutas <tutasg@comcast.net>
2013-06-16 12:18:42 -0400

    add user following

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 SampleApp::Application.routes.draw do
+ 
+ resources :users do
+    member do
+      get :following, :followers
+    end
+  end
 
-  resources :users
 
   resources :sessions, only: [:new, :create, :destroy]
   resources :microposts, only: [:create, :destroy]
+  resources :relationships, only: [:create, :destroy]
 
   match '/signin',  to: 'sessions#new'
   match '/signout', to: 'sessions#destroy', via: :delete
